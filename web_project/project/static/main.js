b4ed0b4b78fc3767f7f60e0e073919bd65c03e3a
hessaawm <hessaawm@gmail.com>
2024-08-15 22:49:50 +0200

    Added initial Django view for index page

--- web_project/project/static/main.js
+++ web_project/project/static/main.js
@@ -1,43 +1,87 @@
-$(document).ready(function(){
-    $("#circlePress").click(function(){
-        $("#toggleCircle").slideToggle("slow");
+// When the document is ready, execute the following code
+$(document).ready(function() {
+    // Circle calculations
+    // -------------------
+    // Toggle circle calculations on click
+    $("#circlePress").click(function() {
+      // Slide toggle the circle calculations container
+      $("#toggleCircle").slideToggle("slow");
     });
-    $("#button1").click(function(){
-        let pi = 3.14;
-        let userQatarCircle = parseFloat($("#userInput").val());
-        let circleArea = pi * (userQatarCircle ** 2);
-        let circlePerimeter = 2 * pi * userQatarCircle;
-        $("#circleArea").val(circleArea);
-        $("#circlePerimeter").val(circlePerimeter);
+  
+    // Calculate circle area and perimeter on button click
+    $("#button1").click(function() {
+      // Define pi constant
+      let pi = 3.14;
+      // Get user input for circle radius
+      let userRadiusCircle = parseFloat($("#userInput").val());
+      // Calculate circle area
+      let circleArea = pi * (userRadiusCircle ** 2);
+      // Calculate circle perimeter
+      let circlePerimeter = 2 * pi * userRadiusCircle;
+      // Display calculated values in input fields
+      $("#circleArea").val(circleArea);
+      $("#circlePerimeter").val(circlePerimeter);
     });
-    $("#rectanglePress").click(function(){
-        $("#toggleRectangle").slideToggle("slow");
+  
+    // Rectangle calculations
+    // ----------------------
+    // Toggle rectangle calculations on click
+    $("#rectanglePress").click(function() {
+      // Slide toggle the rectangle calculations container
+      $("#toggleRectangle").slideToggle("slow");
     });
-    $("#button2").click(function(){
-        let userLengthRectangle = parseFloat($("#rectangleInputLength").val());
-        let userWidthRectangle = parseFloat($("#rectangleInputWidth").val());
-        let rectangleArea = userLengthRectangle * userWidthRectangle;
-        let rectanglePerimeter = 2 * (userLengthRectangle + userWidthRectangle);
-        $("#rectangleArea").val(rectangleArea);
-        $("#rectanglePerimeter").val(rectanglePerimeter);
+  
+    // Calculate rectangle area and perimeter on button click
+    $("#button2").click(function() {
+      // Get user input for rectangle length and width
+      let userLengthRectangle = parseFloat($("#rectangleInputLength").val());
+      let userWidthRectangle = parseFloat($("#rectangleInputWidth").val());
+      // Calculate rectangle area
+      let rectangleArea = userLengthRectangle * userWidthRectangle;
+      // Calculate rectangle perimeter
+      let rectanglePerimeter = 2 * (userLengthRectangle + userWidthRectangle);
+      // Display calculated values in input fields
+      $("#rectangleArea").val(rectangleArea);
+      $("#rectanglePerimeter").val(rectanglePerimeter);
     });
-    $("#squarePress").click(function(){
-        $("#toggleSquare").slideToggle("slow");
+  
+    // Square calculations
+    // -----------------
+    // Toggle square calculations on click
+    $("#squarePress").click(function() {
+      // Slide toggle the square calculations container
+      $("#toggleSquare").slideToggle("slow");
     });
-    $("#button3").click(function(){
-        let userSideSquare = parseFloat($("#squareInputSide").val());
-        let squareArea = userSideSquare ** 2;
-        let squarePerimeter = 4 * userSideSquare;
-        $("#squareArea").val(squareArea);
-        $("#squarePerimeter").val(squarePerimeter);
+  
+    // Calculate square area and perimeter on button click
+    $("#button3").click(function() {
+      // Get user input for square side length
+      let userSideSquare = parseFloat($("#squareInputSide").val());
+      // Calculate square area
+      let squareArea = userSideSquare ** 2;
+      // Calculate square perimeter
+      let squarePerimeter = 4 * userSideSquare;
+      // Display calculated values in input fields
+      $("#squareArea").val(squareArea);
+      $("#squarePerimeter").val(squarePerimeter);
     });
-    $("#trianglePress").click(function(){
-        $("#toggleTriangle").slideToggle("slow");
+  
+    // Triangle calculations
+    // --------------------
+    // Toggle triangle calculations on click
+    $("#trianglePress").click(function() {
+      // Slide toggle the triangle calculations container
+      $("#toggleTriangle").slideToggle("slow");
     });
-    $("#button4").click(function(){
-        let userBaseTriangle = parseFloat($("#triangleInputBase").val());
-        let userHeightTriangle = parseFloat($("#triangleInputHeight").val());
-        let triangleArea = (userBaseTriangle * userHeightTriangle) / 2;
-        $("#triangleArea").val(triangleArea);
+  
+    // Calculate triangle area on button click
+    $("#button4").click(function() {
+      // Get user input for triangle base and height
+      let userBaseTriangle = parseFloat($("#triangleInputBase").val());
+      let userHeightTriangle = parseFloat($("#triangleInputHeight").val());
+      // Calculate triangle area
+      let triangleArea = (userBaseTriangle * userHeightTriangle) / 2;
+      // Display calculated value in input field
+      $("#triangleArea").val(triangleArea);
     });
-});
+  });
--- web_project/project/views.py
+++ web_project/project/views.py
@@ -1,5 +1,12 @@
+# Import necessary modules from Django
 from django.shortcuts import render
 from django.http import HttpResponse
+
+# Define a view function for the index page
 def index(request):
+    # Render the index.html template and return an HTTP response
+    # This function will be called when the user visits the root URL of the site
     return render(request, 'index.html')
+
+# This is where you can define additional views for your Django application
 # Create your views here.
